9d79e4ba3042322fb27fbe1820a6c6a18d382ebd
RendeR <93490632+RendeR66@users.noreply.github.com>
2022-11-11 13:45:16 +0300

    Add files via upload

---
+++ 12.11/BNDProd2d/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Math;
+
+namespace BNDProd2d
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void but_Click(object sender, RoutedEventArgs e)
+        {
+            double x = Convert.ToDouble(Txt_x_1.Text);
+            double a = Convert.ToDouble(Txt_a_1.Text);
+            double y = Convert.ToDouble(Txt_y_1.Text);
+            double b = Convert.ToDouble(Txt_b_1.Text);
+            double z = -999;
+            if (3 <= x && x <= 4)
+                z = Min(a, Max(x, Max(y, b)));
+            else
+                z = Pow(x, 3) - y * Sqrt(b) * Pow(Log(Abs(x)), 2);   
+            outin.Content = String.Format("{0:f4}", z);
+        }
+
+        private void but_Click_2(object sender, RoutedEventArgs e)
+        {
+            double x = Convert.ToDouble(Txt_x_1_Copy1.Text);
+            double c = Convert.ToDouble(Txt_c_1_Copy1.Text);
+            double b = Convert.ToDouble(Txt_b_1_Copy1.Text);
+            double z = -999, cx = c*x;
+            if (0 <= x && x <= 1)
+                z = Cos(1 - (Pow (cx, 2) / b));
+            else if (x > 1)
+                z = Max(c, Max(x, Min(Sqrt(x), Min(Sqrt(b), c))));
+            else if (x < 0)
+                z = Exp(b * x + c);
+            outin1.Content = String.Format("{0:f4}", z);
+        }
+    }
+}
---
+++ 12.11/BNDProd_list/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Math;
+namespace BNDProd_list
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double a = Convert.ToDouble(box1.Text);
+            double b = Convert.ToDouble(box2.Text);
+            double h = Convert.ToDouble(box3.Text);
+            double x = a;
+            int n = Convert.ToInt32(Round((b - a) / h) + 1);
+            listbox11.Items.Clear();
+            listbox11.Items.Add("     x   " + "          y     ");
+            for (int i = 1; i <= n; i++)
+            {
+                double y = Cos(x) + Atan(x);
+                listbox11.Items.Add("     ");
+                listbox11.Items.Add("    " + x.ToString() + "          " + y.ToString());
+                x = x + h;
+            }
+
+
+
+        }
+    }
+}
